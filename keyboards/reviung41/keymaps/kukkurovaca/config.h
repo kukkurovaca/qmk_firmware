fbafaa0d5f1291e96de34481d9e3e7c6fe31e317
kukkurovaca <kukkurovaca@gmail.com>
2022-01-05 23:30:27 -0800

    removing hold on other

--- keyboards/reviung41/keymaps/kukkurovaca/config.h
+++ keyboards/reviung41/keymaps/kukkurovaca/config.h
@@ -148,5 +148,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Tap-hold configuration settings */
 #define PERMISSIVE_HOLD
-#define HOLD_ON_OTHER_KEY_PRESS
 #define IGNORE_MOD_TAP_INTERRUPT
