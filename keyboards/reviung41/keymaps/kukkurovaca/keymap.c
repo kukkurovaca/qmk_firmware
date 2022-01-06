dcf3711fa39d64aa4a93f8f8d61806ecf6836fe5
kukkurovaca <kukkurovaca@gmail.com>
2022-01-05 22:26:31 -0800

    Changing escapefrom alt to control, adding emoji picker on layer, changing leftmost thumb key to alt.

--- keyboards/reviung41/keymaps/kukkurovaca/keymap.c
+++ keyboards/reviung41/keymaps/kukkurovaca/keymap.c
@@ -24,16 +24,16 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
-    LALT_T(KC_ESC)    ,  KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     RALT_T(KC_MINS),
+    LCTL_T(KC_ESC)    ,  KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     RALT_T(KC_MINS),
     LT(_RAISE, KC_TAB),  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  LT(_RAISE, KC_QUOT),
     KC_LSFT           ,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
-                                            KC_LGUI,   KC_BSPC,  MO(_LOWER),  KC_SPC ,    RCTL_T(KC_ENT)
+                                            KC_LALT,   KC_BSPC,  MO(_LOWER),  KC_SPC ,    RCTL_T(KC_ENT)
   ),
 
   [_LOWER] = LAYOUT_reviung41(
 		RESET   , G(KC_TAB), KC_F3   , KC_F4   , KC_F5   , KC_F6	         ,    KC_PSCR , KC_VOLU  , KC_HOME , KC_PGUP , KC_F11   , KC_NO  ,
 		KC_NO	, C(KC_1)  , C(KC_2) , C(KC_3) , C(KC_4) , C(KC_9)           ,    KC_NO   , KC_VOLD  , KC_END  , KC_PGDN , KC_NO   , KC_NO   ,
-		KC_NO   , KC_NO    , KC_NO   , KC_CAPS , KC_NO   , KC_NO             ,    KC_NO   , KC_MUTE  , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+		KC_NO   , RGB_TOG  , KC_NO   , KC_CAPS , KC_NO   , LGUI(KC_DOT)    ,    KC_NO   , KC_MUTE  , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
                                             _______,   MO(_ADJUST),  KC_ENT,   _______,  _______
   ),
 
