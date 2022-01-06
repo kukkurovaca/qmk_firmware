45b086e058fc6e3991b98920d08049a331f0008c
kukkurovaca <kukkurovaca@gmail.com>
2022-01-05 22:19:16 -0800

    Switching escape mod-tap to control, adding emoji picker on function layer.

--- keyboards/ristretto/keymaps/kukkurovaca/keymap.c
+++ keyboards/ristretto/keymaps/kukkurovaca/keymap.c
@@ -25,7 +25,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT(
-		LALT_T(KC_ESC)      , KC_Q      , KC_W      , KC_E    , KC_R    , KC_T              , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , RALT_T(KC_MINS) ,
+		LCTL_T(KC_ESC)      , KC_Q      , KC_W      , KC_E    , KC_R    , KC_T              , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , RALT_T(KC_MINS) ,
 		LT(_RAISE, KC_TAB)  , KC_A      , KC_S      , KC_D    , KC_F    , KC_G              , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , LT(_RAISE, KC_QUOT) ,
 		KC_LSFT             , KC_Z      , KC_X      , KC_C    , KC_V    , KC_B              , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , RSFT_T(KC_ENT) ,
 		KC_LCTL             , MO(_RAISE), MO(_LOWER),KC_LALT,KC_ENT  , KC_BSPC , KC_MUTE , KC_SPC  , KC_RSFT ,MO(_RAISE),KC_RALT , KC_RGUI , RCTL_T(KC_ENT)
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_LOWER] = LAYOUT(
 		RESET   , G(KC_TAB), KC_F3   , KC_F4   , KC_F5   , KC_F6	         ,    KC_PSCR , KC_VOLU  , KC_HOME , KC_PGUP , KC_F11   , KC_NO  ,
 		KC_NO	, C(KC_1)  , C(KC_2) , C(KC_3) , C(KC_4) , C(KC_9)           ,    KC_NO   , KC_VOLD  , KC_END  , KC_PGDN , KC_NO   , KC_NO   ,
-		KC_NO   , KC_NO    , KC_NO   , KC_CAPS , KC_NO   , KC_NO             ,    KC_NO   , KC_MUTE  , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+		KC_NO   , KC_NO    , KC_NO   , KC_CAPS , KC_NO   , LGUI(KC_DOT)             ,    KC_NO   , KC_MUTE  , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
 		KC_TRNS , KC_TRNS  , KC_TRNS , KC_NO   , KC_ENT  , KC_NO   , KC_MUTE ,    KC_NO   , KC_RSFT  ,MO(_ADJUST),KC_TRNS, KC_TRNS , KC_TRNS
 	),
 	[_ADJUST] = LAYOUT(
