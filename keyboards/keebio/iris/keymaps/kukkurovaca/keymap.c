34a95c908837c00717c61bf21eb1117d0f43e639
kukkurovaca <kukkurovaca@gmail.com>
2023-12-06 00:28:58 -0800

    updating reset/reboot

--- keyboards/keebio/iris/keymaps/kukkurovaca/keymap.c
+++ keyboards/keebio/iris/keymaps/kukkurovaca/keymap.c
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET,  G(KC_TAB), KC_NO,   KC_NO,   KC_NO,    KC_NO,                            KC_PSCR, KC_VOLU, KC_HOME, KC_PGUP, KC_F11,  DT_UP,
+     QK_RBT,  G(KC_TAB), KC_NO,   KC_NO,   KC_NO,    KC_NO,                            KC_PSCR, KC_VOLU, KC_HOME, KC_PGUP, KC_F11,  DT_UP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_TOG, C(KC_1), C(KC_2), C(KC_3), C(KC_4), C(KC_9),                            KC_NO,  KC_VOLD, KC_END,    KC_PGDN, KC_NO,  DT_PRNT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_NO,    KC_NO,  KC_CAPS, KC_NO, LGUI(KC_DOT), KC_NO,          KC_NO,  KC_MUTE, KC_NO,    KC_NO,   KC_NO,   KC_NO,   DT_DOWN,
+     RGB_MOD, KC_NO,    KC_NO,  KC_CAPS, KC_NO, LGUI(KC_DOT), QK_BOOT,        QK_BOOT,  KC_MUTE, KC_NO,    KC_NO,   KC_NO,   KC_NO,   DT_DOWN,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     RGB_HUI, RGB_SAI, RGB_VAI,                   RGB_HUD, RGB_SAD, RGB_VAD
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
