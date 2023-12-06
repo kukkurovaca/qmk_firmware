018fe12cf3e787675d165f96b355744040461d80
kukkurovaca <kukkurovaca@gmail.com>
2023-12-05 21:54:01 -0800

    Adding Iris

--- keyboards/ristretto/keymaps/kukkurovaca/keymap.c
+++ keyboards/ristretto/keymaps/kukkurovaca/keymap.c
@@ -28,12 +28,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LCTL_T(KC_ESC)      , KC_Q      , KC_W      , KC_E    , KC_R    , KC_T              , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , RALT_T(KC_MINS) ,
 		LT(_RAISE, KC_TAB)  , KC_A      , KC_S      , KC_D    , KC_F    , KC_G              , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , LT(_RAISE, KC_QUOT) ,
 		KC_LSFT             , KC_Z      , KC_X      , KC_C    , KC_V    , KC_B              , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , RSFT_T(KC_ENT) ,
-		KC_LCTL             , MO(_RAISE), MO(_LOWER),KC_LALT,KC_ENT  , KC_BSPC , KC_MUTE , KC_SPC  , KC_RSFT ,MO(_RAISE),KC_RALT , KC_RGUI , RCTL_T(KC_ENT)
+		KC_LCTL             , MO(_RAISE), MO(_LOWER),KC_LALT,KC_ENT  , KC_BSPC , KC_MUTE , KC_SPC  , KC_RSFT ,KC_RCTL ,KC_RALT , KC_RGUI , RCTL_T(KC_ENT)
 	),
 	[_RAISE] = LAYOUT(
 		KC_GRV	, S(KC_1)   , KC_UP      , S(KC_2) , S(KC_4)   , S(KC_5)        , S(KC_7) , KC_7    , KC_8    , KC_9    , KC_PAST , KC_PSLS ,
 		KC_CAPS	, KC_LEFT   , KC_DOWN    , KC_RIGHT, S(KC_9)   , S(KC_0)        , S(KC_6) , KC_4    , KC_5    , KC_6    , KC_PMNS , KC_EQL  ,
-		KC_TRNS	, S(KC_LBRC), S(KC_RBRC) , S(KC_3) , KC_LBRC   , KC_RBRC        , KC_P0   , KC_1    , KC_2    , KC_3    , KC_PPLS , KC_BSLS ,
+		KC_TRNS	, S(KC_LBRC), S(KC_RBRC) , S(KC_3) , KC_LBRC   , KC_RBRC        , KC_F2   , KC_1    , KC_2    , KC_3    , KC_PPLS , KC_BSLS ,
 		KC_TRNS , KC_TRNS   , KC_TRNS ,MO(_ADJUST),KC_ENT   , KC_DEL  , KC_MPLY , KC_P0   , KC_P0   , KC_PDOT , KC_PDOT , KC_P0  , KC_BSLS
 	),
 	[_LOWER] = LAYOUT(
